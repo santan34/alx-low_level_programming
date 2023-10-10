fe7b2f6947905918a2556572ab7849dca9243af4
St santan <aaronndas@gmail.com>
2023-10-09 17:15:17 -0700

    33

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-
-/**
- *
- *
- *
- *
- *
+/** 
+ * * main - Entry point 
+ * 
+ *code toprint more
+ * 
+ * Return: Always 0 (Success)
+ * 
  */
-
 int main()
 {
 	 int i;
-        for (i=65;i<123;i++)
+        for (i = 65; i< 123; i++)
         {
-
                 putchar(i);
         }
+	putchar('\n');
+	return (0);
 }
