1bf9072aa1269f1738edf61646c4a7ea14d0f43d
St santan <aaronndas@gmail.com>
2023-10-11 03:02:54 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower(int c) - prints out if input is in upper or lower case
+ *
+ * Return: On success 1
+ * On error, 0 is returned, and errno is set appropriately.
+ */
+
+int _islower(int c)
+{
+	int i;
+	int d = 0;
+	for (i = 97; i <= 122; ++i)
+	{
+        	if (c == i)
+		{
+			d = 1;
+		}	
+	}
+	return (d);
+}
