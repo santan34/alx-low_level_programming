27e003b8efcad709d342fc34a3dbf2243c62e2b5
St santan <aaronndas@gmail.com>
2023-10-13 15:23:22 -0700

    rest

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*
+*print_numbers - prints out all single didgit numbers
+*
+* Return: void
+*
+*/
+void print_numbers(void)
+{
+	char c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
