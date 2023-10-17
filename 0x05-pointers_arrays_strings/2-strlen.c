5dea7d1d992b70f4418d9d0b55fef832debff534
St santan <aaronndas@gmail.com>
2023-10-17 15:48:45 -0700

    1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+
+	int i;
+    	int count;
+    	for (i = 0; s[i] != '\0'; i++)
+    	{
+        	count=i;
+	}
+	return (count+1);
+}
