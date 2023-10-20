f014e7be08fca345f65162ef3e3d1d1372503ff7
St santan <aaronndas@gmail.com>
2023-10-20 10:10:10 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,27 @@
 /**
- * _strcat - concatinate two strings
+ *_strncat concatinates two strings up to n
  *
  * @dest:the value src will be concatiiinated to
  * @src: the value to be concatinated to dest
+ * @n :the value we concatinate to
  *
  * Return :the varibale dest is returned0-strcat.c
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	char *temp;
-
+	int i;
+	
 	temp = dest;
 	while (*temp != '\0')
 	{
 		temp++;
 	}
-	while (*src != '\0')
+	for (i = 0; i < n; i++)
 	{
 		*temp = *src;
 		src++;
 		temp++;
-
 	}
 	*temp = '\0';
 	return (dest);
