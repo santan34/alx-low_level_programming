5b6ff8754abde24053675d5ab1d4a3d1d2e62dc6
St santan <aaronndas@gmail.com>
2023-10-26 17:23:06 -0700

    160

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,35 @@
 /**
+ * guesser - helps decide if i'm right
+ * @i: integer to guess
+ * @n: integer to get root of
+ * Return: value of root
+ */
+int guesser(int i, int n)
+{
+	int j;
+
+	if (i * i != n)
+	{
+		if (i > n)
+		{
+			return (-1);
+		}
+		j = guesser(i + 1, n);
+		return (j + 1);
+	}
+	return (0);
+}
+/**
  * _sqrt_recursion - returns square root
  * @n: integer to return
  * Return: returns int of squareroot
  */
 int _sqrt_recursion(int n)
 {
+	int i = 0;
 
+	if (guesser(i, n) == n && n != 1)
+		return (-1);
+	return (guesser(i, n));
+
+}
