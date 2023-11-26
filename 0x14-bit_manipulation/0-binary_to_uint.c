0d95507939658d3981b842181a99756b43ff0286
St santan <aaronndas@gmail.com>
2023-11-26 03:55:23 -0800

    perfect zero

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,27 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal;
-	unsigned int i;
+	int i;
+	int sum = 0;
 
-	for (decimal = 0, i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] == '1')
-			decimal = (decimal << 1) | 1;
-		else if (b[i] == '0')
-			decimal <<= 1;
-		else if (b[i] != '0' && b[i] != '1')
+		if (b == NULL)
+		{
 			return (0);
+		}
+		else if (b[i] == '1')
+		{
+			sum = sum + (1 << i);
+		}
+		else if (b[i] == '0')
+		{
+			sum = sum + (0 << i);
+		}
+		else
+		{
+			return (0);
+		}
 	}
-
-	return (decimal);
+	return (sum);
 }
