12a4ff6b463465777b165de62b3c3d8d8fb764ca
St santan <aaronndas@gmail.com>
2023-11-29 08:06:01 -0800

    2-3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,11 @@
-#ifndef MAIN_H_
-#define MAIN_H_
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stddef.h>
+int append_text_to_file(const char *filename, char *text_content);
+
