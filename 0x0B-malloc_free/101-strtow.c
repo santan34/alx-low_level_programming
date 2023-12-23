be96ea9d69c3476c32f19ccaa786b832270f9ceb
St santan <aaronndas@gmail.com>
2023-12-23 15:27:22 -0800

    trial

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h> 
+/**
+ * strtow -  my own strtok
+ * @str: the string to be split
+ * REturn: a char ** of the tokens
+ */
+char **strtow(char *str)
+{
+	int i = 0;
+	int count = 0;
+	int index = 0;
+	char **p = NULL;
+	int len = strlen(str);
+	int lenth = 0;
+
+	count = counter(str);
+	p = malloc(sizeof(char *) * (count + 1));
+	for (i = 0; i <= len; i++)
+	{
+		lenth = 0;
+		while (str[i] == ' ' && str[i] != '\0')
+		{
+			i += 1;
+		}
+		while (str[i] != ' ' && str[i] != '\0')
+		{
+			lenth += 1;
+			i += 1;
+		}
+		p[index] = malloc(sizeof(char) * lenth);
+		index += 1;
+	}	
+	return(p);
+
+}
+
+
+int counter(char *str)
+{
+	int count = 0;
+	int flag = 0;
+	int i = 0;
+	int len = strlen(str);
+
+	for (i = 0; i <= len ; i++)
+	{
+
+		while(str[i] == ' ' && str[i] != '\0')
+		{
+			i += 1;
+		}
+		while (str[i] != ' ' && str[i] != '\0')
+		{
+			flag = 1;
+			i += 1;
+		}
+		if (flag == 1)
+		{
+			count += 1;
+		}
+		flag = 0;
+
+	}
+	return (count);
+}
