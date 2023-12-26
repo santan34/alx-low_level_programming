fb6fff2e81536ef77a642814c0b7aaa2c3ac9bde
St santan <aaronndas@gmail.com>
2023-12-26 08:15:11 -0800

    numbe 1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - function to allocate memory plus null check
+ * @b: the number of bytes to allocate
+ * Return : void pinter
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *ret = NULL;
+
+	ret = malloc(b);
+	if (ret == NULL)
+	{
+		exit(98);
+	}
+	return (ret);
+}
