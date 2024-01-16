a9c7506045479ce0717404a16ca6fbb711de4aca
St <takundaveni@gmail.com>
2024-01-15 18:24:46 -0800

    libs1

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,16 @@
+/**
+ * _memcpy - copies from src to dest
+ * @dest: destination to copy to
+ * @src: source to copy from
+ * @n: n amount of times
+ * Return: return the string value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
