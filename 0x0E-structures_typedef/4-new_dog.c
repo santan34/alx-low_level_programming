bcb5337205dc40f8499a5147503059158dd2c923
St <takundaveni@gmail.com>
2024-01-16 17:44:03 -0800

    444

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,16 +8,18 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *cpy_name = strdup(name);
-	char *cpy_owner = strdup(owner);
+	char *cpy_name;
+	char *cpy_owner;
 	dog_t *nw;
-	
+
+	cpy_name = strdup(name);
 	if (cpy_name == NULL)
 	{
 		free(cpy_name);
 		free(cpy_owner);
 		return (NULL);
 	}
+	cpy_owner = strdup(owner);
 	if (cpy_owner == NULL)
 	{
 		free(cpy_name);
