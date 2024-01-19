1e18adeb3f16d94e9f56d9b265c6477457f98871
St <takundaveni@gmail.com>
2024-01-19 14:11:56 -0800

    4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 /**
  * free_list - free a singly linked list
- * @Return: void
+ * @head: points to the first element of the list
+ * Return: void
  */
 void free_list(list_t *head)
 {
 	list_t *del = NULL;
 
-	while (head != NULL )
+	while(head != NULL )
 	{
 		del = head->next;
 		free(head->str);
