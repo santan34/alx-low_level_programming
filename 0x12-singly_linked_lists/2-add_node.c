8bc4aa59244279b2738f07d366c416de8ff195ff
St <takundaveni@gmail.com>
2024-01-19 05:21:25 -0800

    c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - add a node to a struct
+ * @head: a pointer to a pointer to a struct
+ * @str: a string to be added to a node
+ * Return: a pointer to a struct
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = NULL;
+	char *string = NULL;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	string = strdup(str);
+	if (string == NULL)
+		return (NULL);
+	new->str = string;
+	new->len = strlen(string);
+	new->next = *head;
+	*head = new;
+	return (*head);
+
+}
