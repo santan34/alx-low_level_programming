ae56a87431b0ab73f4574dfa02495e228cb67de6
St <takundaveni@gmail.com>
2024-01-19 14:48:27 -0800

    50

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,32 +8,40 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *cpy_name;
-	char *cpy_owner;
-	dog_t *nw;
+	int i, len1, len2;
+	dog_t *ndog;
+	char *a, *b;
 
-	cpy_name = strdup(name);
-	if (cpy_name == NULL)
+	len1 = 0;
+	for (i = 0; name[i] != '\0'; i++)
+		len1++;
+	len2 = 0;
+	for (i = 0; owner[i] != '\0'; i++)
+		len2++;
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+	a = malloc((len1 + 1) * sizeof(char));
+	if (a == NULL)
 	{
-		free(cpy_name);
+		free(ndog);
 		return (NULL);
 	}
-	cpy_owner = strdup(owner);
-	if (cpy_owner == NULL)
+	for (i = 0; i < len1; i++)
+		a[i] = name[i];
+	a[i] = '\0';
+	ndog->name = a;
+	ndog->age = age;
+	b = malloc((len2 + 1) * sizeof(char));
+	if (b == NULL)
 	{
-		free(cpy_name);
-		free(cpy_owner);
+		free(a);
+		free(ndog);
 		return (NULL);
 	}
-	nw = malloc(sizeof(dog_t));
-	if (nw == NULL)
-	{
-		free(cpy_name);
-		free(cpy_owner);
-		return (NULL);
-	}
-	nw->name = cpy_name;
-	nw->age = age;
-	nw->owner = cpy_owner;
-	return (nw);
+	for (i = 0; i < len2; i++)
+		b[i] = owner[i];
+	b[i] = '\0';
+	ndog->owner = b;
+	return (ndog);
 }
