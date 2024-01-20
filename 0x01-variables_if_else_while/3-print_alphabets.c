712fc137482c79add1e7e158513b4f9bf52113e6
St <takundaveni@gmail.com>
2024-01-19 19:10:43 -0800

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-/** 
- * * main - Entry point 
- * 
- *code toprint more
- * 
+/**
+ * main - Entry point
  * Return: Always 0 (Success)
- * 
  */
-int main()
+int main(void)
 {
-	 int i;
-        for (i = 123; i > 65; i--)
-        {	
-		while ((i < 97)&& (i > 90))	
+	int i = 0;
+
+	for (i = 97; i < 123; i++)
+	{
 		putchar(i);
-        
+	}
+	for (i = 65; i < 91; i++)
+	{
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
