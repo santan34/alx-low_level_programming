83897b3f8e25076c7805e1fcec2823c056c3ef28
St <takundaveni@gmail.com>
2024-03-20 17:11:00 -0700

    fixed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 /**
- * main - Entry point  
- * code to decide if a random number is even or odd 
- *  Return: Always 0 (Success) 
+ * main - Entry point
+ * code to decide if a random number is even or odd
+ * Return: Always 0 (Success)
  */
-
-int main()
+int main(void)
 {
 	int ch;
-	for (ch = 97; ch<=122; ch++)
+
+	for (ch = 97; ch <= 122; ch++)
 	{
 		if (ch == 113)
 			ch += 1;
-		if  (ch  == 101)
+		if (ch  == 101)
 			ch += 1;
 		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
 }
-
