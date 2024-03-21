44e2c20c21a2ce6a32dfd4246b6dd6767fa2c887
St <takundaveni@gmail.com>
2024-03-20 18:14:01 -0700

    2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
- *
  * print_alphabet - prints out all the letters of the alphabet in small letters
- *
  * Return: On success 0
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 void print_alphabet(void)
 {
-    	int i;
-    	for (i=97;i<=122;++i)
-   	{	
-        	_putchar(i);
-    	}
-    	_putchar('\n');
+	int i;
+
+	for (i = 97; i <= 122; ++i)
+   	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
