891838e4d603b7e38c1a8145454a71cf63dcbce6
St <takundaveni@gmail.com>
2024-03-20 17:15:16 -0700

    add numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+/*
+ * main - Prints all single digit numbers of base 10
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
