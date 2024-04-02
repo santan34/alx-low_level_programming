f0b579b0128602fe7434ebae608d7e1f33878b65
St <takundaveni@gmail.com>
2024-04-01 19:04:40 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
  *  _isalpha - prints out if input is in upper or lower case
+ *  @c: the number
  *  Return: On success 1
  */
 int _isalpha(int c)
 {
 	int i;
 	int d = 0;
+
 	for (i = 97; i <= 122; ++i)
 	{
 		if (c == i)
