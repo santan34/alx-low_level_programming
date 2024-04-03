09e587becbfe75b05c48cf8711567a08917ee603
St <takundaveni@gmail.com>
2024-04-03 09:33:43 -0700

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,9 @@
 #include "main.h"
-
 /**
-*
-* _isdigit - function that check for a dgit.
-*
-* @c: char to check
-*
-* Return:  0 or 1
-*
-**/
+ * _isdigit - function that check for a dgit.
+ * @c: char to check
+ * Return:  0 or 1
+ */
 
 int _isdigit(int c)
 {
