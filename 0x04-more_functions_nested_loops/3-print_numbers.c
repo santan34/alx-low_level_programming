29b1a9108dc4f7f7838e064673c484ac9506a3b6
St <takundaveni@gmail.com>
2024-04-03 09:47:55 -0700

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,12 @@
 #include "main.h"
-
 /**
-*
-*print_numbers - prints out all single didgit numbers
-*
-* Return: void
-*
-*/
+ * print_numbers - prints out all single didgit numbers
+ * Return: void
+ */
 void print_numbers(void)
 {
 	char c;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
