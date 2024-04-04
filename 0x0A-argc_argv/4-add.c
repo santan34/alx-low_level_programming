4c26411937e8cb26345ff4a3fecc3a31238e76a9
St <takundaveni@gmail.com>
2024-04-04 13:29:07 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,47 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#define UNUSED(x)(void)(x)
 /**
- * main - adds positive numbers
- * @argc: the number of argumnents
- * @argv: the arguments themselves
- * Return: 0 on success, 1 on Error
+ * main - adds two numbers
+ * @argc:int
+ * @argv:array of strings
+ * Return: sum if all digits
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, sum, x;
+	int i;
+	int j;
+	int c;
+	int sum;
+	char *s;
 
-	x = 0;
-	if ((argc - 1) == 0)
+	sum = 0;
+	c = 0;
+
+	i = 1;
+	while (i < argc)
 	{
-		printf("0\n");
-		return (1);
+		s = argv[i];
+		j = 0;
+		while (s[j] != '\0')
+		{
+			if (s[j] < '0' || s[j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		i++;
 	}
-	else
+
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			if (atoi(argv[i]) != 0)
-			{
-				if (atoi(argv[i]) > 0)
-				{
-					sum += atoi(argv[i]);
-				}
-			}
-			else
-			{
-				x = 1;
-			}
-		}
-		if (x == 1)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			printf("%d\n", sum);
-			return (0);
-		}
+		c = atoi(argv[i]);
+		sum = sum + c;
 	}
+	printf("%d\n", sum);
+	return (0);
 }
