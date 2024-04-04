d8b2861bbc0c4d683457dbaf0700705dca7ef7a7
St <takundaveni@gmail.com>
2024-04-04 14:38:53 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,5 @@
+#undef MAIN_H
+#define MAIN_H
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 #include <stdio.h>
@@ -8,4 +10,4 @@ int create_file(const char *filename, char *text_content);
 #include <unistd.h>
 #include <stddef.h>
 int append_text_to_file(const char *filename, char *text_content);
-
+#unde
