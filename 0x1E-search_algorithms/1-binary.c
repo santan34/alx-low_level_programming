9c3b8cbbb9244d24ee8182b5618811407dc5a04b
root <takundaveni@gmail.com>
2024-05-07 18:27:14 -0700

    14

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -20,7 +20,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		mid = (lo + hi) / 2;
 		printf("Searching in array: ");
-		for (i = lo; i <= hi; i++)
+		for (i = lo; i != hi; i++)
 		{
 			printf("%d", array[i]);
 			if (i != hi)
