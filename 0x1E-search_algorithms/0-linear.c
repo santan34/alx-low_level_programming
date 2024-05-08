d58c9df8c734abedc0d2fe083a59f34881075bea
root <takundaveni@gmail.com>
2024-05-07 19:33:12 -0700

    000

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,7 +19,10 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i++)
 	{
 		if (array[i] == value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 			return (i);
+		}
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	}
 	return (-1);
